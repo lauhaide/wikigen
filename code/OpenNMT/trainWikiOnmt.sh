e4c526f30a285d7578b54c8fd758b3a8fda95fae
Laura Perez <lperez@pangloss.inf.ed.ac.uk>
2018-06-12 18:02:38 +0100

    update RL scripts

--- code/OpenNMT/trainWikiOnmt.sh
+++ code/OpenNMT/trainWikiOnmt.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=$1 th wiki_train2.lua -gpuid 1 \
 -valid_data_file ../../oTrained/input/$3-valid.hdf5 \
 -data_dict ../../oTrained/input/$3.dbp.dict \
 -text_dict ../../oTrained/input/$3.text.dict \
--preembed -preembed_datavoc ../../vectors/$3.dbp-glove.6B.100dembeddings.t7  \
+-preembed -preembed_datavoc ./../oTrained/input/$3.dbp-glove.6B.100dembeddings.t7  \
 -rnn_size 100 -word_vec_size 100 -enc_emb_size 100 -input_feed 0 -layers 1 \
 -epochs 20 -learning_rate 0.001 -optim adam \
 -save_model ../../oTrained/models/ -max_bptt $2 $4 "
--- code/OpenNMT/trainWikiOnmt_RL.sh
+++ code/OpenNMT/trainWikiOnmt_RL.sh
@@ -4,6 +4,6 @@ CUDA_VISIBLE_DEVICES=$1 th wiki_train2.lua -gpuid 1\
 -valid_data_file ../../oTrained/input/$4-valid.hdf5 \
 -data_dict ../../oTrained/input/$4.dbp.dict \
 -text_dict ../../oTrained/input/$4.text.dict \
--preembed -preembed_datavoc ../../vectors/$4.dbp-glove.6B.100dembeddings.t7  \
+-preembed -preembed_datavoc ./../oTrained/input/$4.dbp-glove.6B.100dembeddings.t7  \
 -rnn_size 100 -word_vec_size 100 -enc_emb_size 100 -input_feed 0 -layers 1 -max_bptt $2 $3 \
 -save_model ../../oTrained/models/
